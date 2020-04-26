4eb5c753c73b6220cbc4d7cb9db9594639f9503a
alexandraivan97 <alexandraivan97@gmail.com>
2020-04-26 20:48:12 +0300

    add new endpoints for interviews

--- src/main/java/com/fmi/aop/service/impl/InterviewServiceImpl.java
+++ src/main/java/com/fmi/aop/service/impl/InterviewServiceImpl.java
@@ -2,24 +2,18 @@ package com.fmi.aop.service.impl;
 
 
 import com.fmi.aop.dto.InterviewDTO;
-import com.fmi.aop.dto.InterviewerDTO;
 import com.fmi.aop.entity.Interview;
-import com.fmi.aop.entity.Interviewer;
 import com.fmi.aop.mapper.InterviewMapper;
-import com.fmi.aop.mapper.InterviewerMapper;
 import com.fmi.aop.repository.InterviewRepository;
-import com.fmi.aop.repository.InterviewerRepository;
 import com.fmi.aop.service.IInterviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.security.InvalidParameterException;
+import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static com.fmi.aop.utils.Constants.CANDIDATE_EMAIL;
-import static com.fmi.aop.utils.Constants.INVALID_PARAMETER_EXCEPTION;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -31,6 +25,18 @@ public class InterviewServiceImpl  implements IInterviewService {
     @Override
     public Set<InterviewDTO> getInterviewByCandidateEmail(String email) {
         return interviewRepository.findInterviewByCandidateEmail(email).stream()
+                .map(this::toInterviewDTO).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<InterviewDTO> getInterviewByDate(LocalDateTime date) {
+        return interviewRepository.findAllByDate(date).stream()
+                .map(this::toInterviewDTO).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<InterviewDTO> getInterviews() {
+        return interviewRepository.findAll().stream()
                 .map(this::toInterviewDTO).collect(Collectors.toSet());
     }
 
